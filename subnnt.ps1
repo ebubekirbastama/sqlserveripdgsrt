9f42ebc0e864c44078e2ebd2d74480915b4d75af
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2023-11-04 21:54:20 +0300

    Update subnnt.ps1

--- subnnt.ps1
+++ subnnt.ps1
@@ -1,12 +1,12 @@
-$adapter = Get-NetAdapter | ? {$_.InterfaceAlias -eq 'Ethernet'} # Adaptör adını değiştirebilirsiniz
+$adapter = Get-NetAdapter -Name "Ethernet" # Adaptör adını değiştirebilirsiniz
 $ipAddress = "192.168.1.40"
 $subnetMask = "255.255.255.0"
 $gateway = "192.168.1.1"
 $dns = "192.168.1.1"
 
-# IP adresini ayarla
-$adapter | Set-NetIPInterface -InterfaceMetric 2
-$adapter | New-NetIPAddress -IPAddress $ipAddress -PrefixLength 24 -DefaultGateway $gateway
+# IP ayarlarını yapılandır
+$adapter | Remove-NetIPAddress -Confirm:$false
+$adapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $ipAddress -PrefixLength 24 -DefaultGateway $gateway
 
-# DNS ayarlarını ayarla
-$adapter | Set-DnsClientServerAddress -ServerAddresses $dns
+# DNS ayarlarını yapılandır
+Set-DnsClientServerAddress -InterfaceIndex $adapter.ifIndex -ServerAddresses $dns
